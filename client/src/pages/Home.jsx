29c7c0da6895ac7b6767bbe4f51168c3c1feb358
Gautam Malviya <gmalviya619@gmail.com>
2025-09-03 18:16:24 +0530

    Home page is completed. Home, hero, car cards, feature section, testimonials, newsletter, footer pages are added in the home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Banner from '../components/Banner'
+import FeaturedSection from '../components/FeaturedSection'
+import Hero from '../components/Hero'
+import Newsletter from '../components/Newsletter'
+import Testimonial from '../components/Testimonial'
+
+const Home = () => {
+  return (
+    <>
+      <Hero/>
+      <FeaturedSection/>
+      <Banner/>
+      <Testimonial/>
+      <Newsletter/>
+    </>
+  )
+}
+
+export default Home
